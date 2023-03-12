240115ede7f498399c418cc34afc39e3a42a52e5
Clement Ramos <ramos.clement@outlook.fr>
2023-03-12 21:33:12 +0100

    re added price

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -7,12 +7,13 @@ import prisma from "../../../lib/prisma";
 // Required fields in body: title
 // Optional fields in body: content
 export default async function handle(req, res) {
-  const { title, content } = req.body;
+  const { title, content, price } = req.body;
 
   const session = await getSession({ req });
   const result = await prisma.post.create({
     data: {
       title: title,
+      price: price,
       content: content,
       author: { connect: { email: session?.user?.email } },
     },
