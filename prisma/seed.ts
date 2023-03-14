e9ba5082bbd7564c0ed4bf9171d08d5ea78c43e2
Clement Ramos <ramos.clement@outlook.fr>
2023-03-14 19:08:44 +0100

    added docs

---
+++ prisma/seed.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+async function create() {
+  await prisma.user.create({
+    data: {
+      email: `test@gmail.com`,
+      role: `ADMIN`,
+    },
+  });
+}
+
+create()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
