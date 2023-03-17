40948648efe001b1e8b4c9f78b46c27d7878d0c9
Clement Ramos <ramos.clement@outlook.fr>
2023-03-17 11:06:19 +0100

    trying again

--- pages/api/hour/[id].ts
+++ pages/api/hour/[id].ts
@@ -1,15 +1,15 @@
-// import prisma from '../../../lib/prisma';
+import prisma from '../../../lib/prisma';
 
-// export default async function handle(req, res) {
-//   const hoursId = req.query.id;
-//   if (req.method === 'DELETE') {
-//     const hours = await prisma.hours.delete({
-//       where: { id: hoursId },
-//     });
-//     res.json(hours);
-//   } else {
-//     throw new Error(
-//       `La requète HTTP : ${req.method} n'est pas supportée ici.`,
-//     );
-//   }
-// }
+export default async function handle(req, res) {
+  const hoursId = req.query.id;
+  if (req.method === 'DELETE') {
+    const hours = await prisma.hours.delete({
+      where: { id: hoursId },
+    });
+    res.json(hours);
+  } else {
+    throw new Error(
+      `La requète HTTP : ${req.method} n'est pas supportée ici.`,
+    );
+  }
+}
